09213d806784793e7b618429eca92e3a3309efd6
Sahil Dadia <dadiasahil94@yahoo.in>
2017-07-31 12:49:29 +0530

    Added files to do common math functions

--- include/math_functions.hpp
+++ include/math_functions.hpp
@@ -3,7 +3,8 @@
 #ifndef _MATH_FUNCTIONS_HPP_
 #define _MATH_FUNCTIONS_HPP_
 
-#include "mkl.h"
+#include <cblas.h>
+//#include "mkl.h"
 
 
 namespace dpl {
@@ -12,24 +13,24 @@ namespace dpl {
     // limitation that the data has to be contiguous in memory.
     template <typename Dtype>
     void dpl_cpu_gemm(const CBLAS_TRANSPOSE TransA,
-                      const CBLAS_TRANSPOSE TransB, 
-                      const int M, 
-                      const int N, 
+                      const CBLAS_TRANSPOSE TransB,
+                      const int M,
+                      const int N,
                       const int K,
-                      const Dtype alpha, 
-                      const Dtype* A, 
-                      const Dtype* B, 
+                      const Dtype alpha,
+                      const Dtype* A,
+                      const Dtype* B,
                       const Dtype beta,
                       Dtype* C);
 
 
     template <typename Dtype>
-    void dpl_cpu_gemv(const CBLAS_TRANSPOSE TransA, 
-                      const int M, 
-                      const int N, 
-                      const Dtype alpha, 
-                      const Dtype* A, 
-                      const Dtype* x, 
+    void dpl_cpu_gemv(const CBLAS_TRANSPOSE TransA,
+                      const int M,
+                      const int N,
+                      const Dtype alpha,
+                      const Dtype* A,
+                      const Dtype* x,
                       const Dtype beta,
                       Dtype* y);
 
@@ -37,8 +38,8 @@ namespace dpl {
 
 
     template <typename Dtype>
-        void dpl_axpy(const int N, 
-                      const Dtype alpha, 
+        void dpl_axpy(const int N,
+                      const Dtype alpha,
                       const Dtype* X,
                       Dtype* Y);
 
@@ -49,14 +50,14 @@ namespace dpl {
 
 
     template <typename Dtype>
-        void dpl_copy(const int N, 
-                      const Dtype *X, 
+        void dpl_copy(const int N,
+                      const Dtype *X,
                       Dtype *Y);
 
 
     template <typename Dtype>
-        void dpl_scal(const int N, 
-                      const Dtype alpha, 
+        void dpl_scal(const int N,
+                      const Dtype alpha,
                       Dtype *X);
 
 
@@ -89,8 +90,8 @@ namespace dpl {
         void dpl_exp(const int n, const Dtype* a, Dtype* y);
 
     template <typename Dtype>
-        Dtype dpl_cpu_dot(const int n, 
-                          const Dtype* x, 
+        Dtype dpl_cpu_dot(const int n,
+                          const Dtype* x,
                           const Dtype* y);
 
 
--- src/math_functions.cpp
+++ src/math_functions.cpp
@@ -1,9 +1,9 @@
 // Copyright 2013 Yangqing Jia
 
-#include "mkl.h"                                  // add intel MKL library later
+//#include "mkl.h"                                  // add intel MKL library later
 #include <iostream>
 #include <math.h>
-//#include <cblas.h>
+#include <cblas.h>
 #include "math_functions.hpp"
 
 namespace dpl {
@@ -11,153 +11,153 @@ namespace dpl {
     template <>
         void dpl_cpu_gemv<float>(const CBLAS_TRANSPOSE TransA,
                                  const int M,
-                                 const int N, 
-                                 const float alpha, 
-                                 const float* A, 
+                                 const int N,
+                                 const float alpha,
+                                 const float* A,
                                  const float* x,
-                                 const float beta, 
+                                 const float beta,
                                  float* y)
         {
             cblas_sgemv(CblasRowMajor, TransA, M, N, alpha, A, N, x, 1, beta, y, 1);
         }
 
     template <>
-        void dpl_cpu_gemv<double>(const CBLAS_TRANSPOSE TransA, 
+        void dpl_cpu_gemv<double>(const CBLAS_TRANSPOSE TransA,
                                   const int M,
-                                  const int N, 
-                                  const double alpha, 
-                                  const double* A, 
+                                  const int N,
+                                  const double alpha,
+                                  const double* A,
                                   const double* x,
-                                  const double beta, 
-                                  double* y) 
+                                  const double beta,
+                                  double* y)
         {
             cblas_dgemv(CblasRowMajor, TransA, M, N, alpha, A, N, x, 1, beta, y, 1);
         }
 
     template<>
         void dpl_cpu_gemm<float>(const CBLAS_TRANSPOSE TransA,
-                                 const CBLAS_TRANSPOSE TransB, 
-                                 const int M, 
-                                 const int N, 
+                                 const CBLAS_TRANSPOSE TransB,
+                                 const int M,
+                                 const int N,
                                  const int K,
-                                 const float alpha, 
-                                 const float* A, 
-                                 const float* B, 
+                                 const float alpha,
+                                 const float* A,
+                                 const float* B,
                                  const float beta,
-                                 float* C) 
+                                 float* C)
         {
           int lda = (TransA == CblasNoTrans) ? K : M;
           int ldb = (TransB == CblasNoTrans) ? N : K;
-          cblas_sgemm(CblasRowMajor, 
-                      TransA, 
-                      TransB, 
-                      M, 
-                      N, 
-                      K, 
-                      alpha, 
-                      A, 
-                      lda, 
+          cblas_sgemm(CblasRowMajor,
+                      TransA,
+                      TransB,
+                      M,
+                      N,
+                      K,
+                      alpha,
+                      A,
+                      lda,
                       B,
-                      ldb, 
-                      beta, 
-                      C, 
+                      ldb,
+                      beta,
+                      C,
                       N);
         }
 
 
     template<>
         void dpl_cpu_gemm<double>(const CBLAS_TRANSPOSE TransA,
-                                 const CBLAS_TRANSPOSE TransB, 
-                                 const int M, 
-                                 const int N, 
+                                 const CBLAS_TRANSPOSE TransB,
+                                 const int M,
+                                 const int N,
                                  const int K,
-                                 const double alpha, 
-                                 const double* A, 
-                                 const double* B, 
+                                 const double alpha,
+                                 const double* A,
+                                 const double* B,
                                  const double beta,
-                                 double* C) 
+                                 double* C)
         {
           int lda = (TransA == CblasNoTrans) ? K : M;
           int ldb = (TransB == CblasNoTrans) ? N : K;
-          cblas_dgemm(CblasRowMajor, 
-                      TransA, 
-                      TransB, 
-                      M, 
-                      N, 
-                      K, 
-                      alpha, 
-                      A, 
-                      lda, 
+          cblas_dgemm(CblasRowMajor,
+                      TransA,
+                      TransB,
+                      M,
+                      N,
+                      K,
+                      alpha,
+                      A,
+                      lda,
                       B,
-                      ldb, 
-                      beta, 
-                      C, 
+                      ldb,
+                      beta,
+                      C,
                       N);
         }
 
 
 
     template <>
-        void dpl_axpy<float>(const int N, 
-                             const float alpha, 
+        void dpl_axpy<float>(const int N,
+                             const float alpha,
                              const float* X,
-                             float* Y) 
-        { 
+                             float* Y)
+        {
             cblas_saxpy(N, alpha, X, 1, Y, 1);
         }
 
     template <>
-        void dpl_axpy<double>(const int N, 
+        void dpl_axpy<double>(const int N,
                               const double alpha,
                               const double* X,
-                              double* Y) 
-        { 
-            cblas_daxpy(N, alpha, X, 1, Y, 1); 
+                              double* Y)
+        {
+            cblas_daxpy(N, alpha, X, 1, Y, 1);
         }
 
     template <>
-        void dpl_copy<float>(const int N, 
-                             const float* X, 
-                             float* Y) 
+        void dpl_copy<float>(const int N,
+                             const float* X,
+                             float* Y)
         {
             cblas_scopy(N, X, 1, Y, 1);
         }
 
     template <>
-        void dpl_copy<double>(const int N, 
-                              const double* X, 
-                              double* Y) 
+        void dpl_copy<double>(const int N,
+                              const double* X,
+                              double* Y)
         {
             cblas_dcopy(N, X, 1, Y, 1);
         }
 
     template <>
-        void dpl_scal<float>(const int N, const float alpha, float *X) 
+        void dpl_scal<float>(const int N, const float alpha, float *X)
         {
             cblas_sscal(N, alpha, X, 1);
         }
 
     template <>
-        void dpl_scal<double>(const int N, const double alpha, double *X) 
+        void dpl_scal<double>(const int N, const double alpha, double *X)
         {
             cblas_dscal(N, alpha, X, 1);
         }
 
     template <>
-        float dpl_cpu_dot<float>(const int n, const float* x, const float* y) 
+        float dpl_cpu_dot<float>(const int n, const float* x, const float* y)
         {
             return cblas_sdot(n, x, 1, y, 1);
         }
 
     template <>
-        double dpl_cpu_dot<double>(const int n, const double* x, const double* y) 
+        double dpl_cpu_dot<double>(const int n, const double* x, const double* y)
         {
             return cblas_ddot(n, x, 1, y, 1);
         }
 
     template<>
         void dpl_sqr<float>(const int N, float* y)
-        {   
+        {
             for(int i=0; i<N ;++i)
             {
                 y[i] = pow(y[i], 2.0);
@@ -167,7 +167,7 @@ namespace dpl {
 
     template<>
         void dpl_sqr<double>(const int N, double* y)
-        {   
+        {
             for(int i=0; i<N ;++i)
             {
                 y[i] = pow(y[i], 2.0);
@@ -312,7 +312,7 @@ int main()
     // find dot prooduct
     std::cout <<  dpl::dpl_cpu_dot(N, ar1, ar2) << std::endl;
 
-    // find square of a vector 
+    // find square of a vector
     dpl::dpl_powx(N, ar1, 1/2.0, ar3);
     for(int i=0; i<N; i++)
         std::cout << ar3[i] << std::endl;
